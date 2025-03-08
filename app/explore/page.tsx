545d7676bd5b87d5bc77ba963764c788aac988e1
EsleySaab <esleysantana.sa@gmail.com>
2025-03-08 19:08:34 -0300

    chore: add shadcn setup

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "../_components/ui/button";
+
 const ExplorePage = () => {
-  return <h1 className="text-2xl">Hello Explore!</h1>;
+  return (
+    <>
+      <h1 className="text-2xl">Hello Explore!</h1>
+      <Button className="bg-gray-600">Avaliar</Button>
+    </>
+  );
 };
 
 export default ExplorePage;
