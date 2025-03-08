02d61dc1b6b0f8f9e308fe7dc288ec32be12b74f
EsleySaab <esleysantana.sa@gmail.com>
2025-03-08 15:33:47 -0300

    chore: configure tailwindcss with custom theme

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,5 +1,5 @@
 const ExplorePage = () => {
-  return <h1>Hello Explore!</h1>;
+  return <h1 className="text-2xl">Hello Explore!</h1>;
 };
 
 export default ExplorePage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,63 +1,79 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", "sans-serif"],
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        md: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+      },
+      fontWeight: {
+        regular: "400",
+        medium: "500",
+        bold: "700",
+      },
+      lineHeight: {
+        shorter: "125%",
+        short: "140%",
+        base: "160%",
+        tall: "180%",
+      },
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        green: {
+          100: "#50B2C0",
+          200: "#255D6A",
+          300: "#0A313C",
+        },
+        purple: {
+          100: "#8381D9",
+          200: "#2A2879",
+        },
+        gray: {
+          100: "#F8F9FC",
+          200: "#E6E8F2",
+          300: "#D1D6E4",
+          400: "#8D95AF",
+          500: "#303F73",
+          600: "#252D4A",
+          700: "#181C2A",
+          800: "#0E1116",
+        },
+        "gradient-vertical":
+          "linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)",
+        "gradient-horizontal":
+          "linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)",
+      },
+      spacing: {
+        px: "1px",
+        1: "0.25rem",
+        2: "0.5rem",
+        3: "0.75rem",
+        4: "1rem",
+        5: "1.25rem",
+        6: "1.5rem",
+        7: "1.75rem",
+        8: "2rem",
+        10: "2.5rem",
+      },
+    },
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
