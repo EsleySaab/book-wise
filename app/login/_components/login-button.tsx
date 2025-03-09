bc2225b6114b050e875679d7635bae4b162a7e90
EsleySaab <esleysantana.sa@gmail.com>
2025-03-09 13:30:05 -0300

    feat: adding login page

---
+++ app/login/_components/login-button.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/app/_components/ui/button";
+import Image from "next/image";
+
+interface LoginButtonProps {
+  image: string;
+  alt: string;
+  text: string;
+}
+
+const LoginButton = ({ text, image, alt }: LoginButtonProps) => {
+  return (
+    <Button className="flex items-center justify-start gap-4 bg-gray-600 p-7">
+      <Image src={image} alt={alt} width={24} height={24} />
+      <p className="font-semibold text-gray-200">{text}</p>
+    </Button>
+  );
+};
+
+export default LoginButton;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,59 @@
-const Login = () => {
-  return <h1>Hello Login!</h1>
-}
+import Image from "next/image";
+import LoginButton from "./_components/login-button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default Login
+const LoginPage = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
+  return (
+    <div className="grid h-full grid-cols-[1fr_2fr] p-5">
+      {/* Esquerda */}
+      <div className="relative h-full w-full">
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
+      </div>
+      {/* Direita */}
+      <div className="mx-auto flex w-[350px] flex-col justify-center">
+        <h1 className="text-2xl font-bold">Boas vindas!</h1>
+        <p className="mb-10 text-gray-300">
+          Faça seu login ou acesse como visitante.
+        </p>
+
+        <div className="flex w-full flex-col space-y-4">
+          <SignInButton>
+            <LoginButton
+              text="Entrar com o Google"
+              image="/google.svg"
+              alt="Logo do Google"
+            />
+          </SignInButton>
+          <SignInButton>
+            <LoginButton
+              text="Entrar com o GitHub"
+              image="/github.svg"
+              alt="Logo do GitHub"
+            />
+          </SignInButton>
+
+          <LoginButton
+            text="Acessar como visitante"
+            image="/visitant.svg"
+            alt="Logo de Visitante"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
