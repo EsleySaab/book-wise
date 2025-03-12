8b879b9202492b6662ea07a50e3a092965a663c9
EsleySaab <esleysantana.sa@gmail.com>
2025-03-12 19:21:16 -0300

    feat: ensuring that popular books have ratings

--- app/(home)/components/popular-books.tsx
+++ app/(home)/components/popular-books.tsx
@@ -3,11 +3,17 @@ import { db } from "@/app/_lib/prisma";
 import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import Rate from "./rate";
 
 const PopularBooks = async () => {
   const books = await db.book.findMany({
+    include: {
+      ratings: true,
+    },
     take: 4,
   });
+
+  console.log(books);
 
   return (
     <div className="w-[324px]">
@@ -24,22 +30,31 @@ const PopularBooks = async () => {
         </div>
       </div>
       <div className="flex flex-col gap-3">
-        {books.map((book) => (
-          <Card key={book.id} className="w-full rounded-lg bg-gray-700 pt-4">
-            <CardContent className="flex gap-5">
-              <Image
-                src={book.cover_url}
-                alt={`Imagem do livro ${book.name}`}
-                width={64}
-                height={94}
-              />
-              <div className="flex flex-col">
-                <h2 className="font-bold text-gray-100">{book.name}</h2>
-                <p className="text-xs text-gray-400">{book.author}</p>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+        {books.map((book) => {
+          const rating = book.ratings[0];
+          const ratingValue = rating ? rating.rate : 0;
+          return (
+            <Card key={book.id} className="w-full rounded-lg bg-gray-700 pt-4">
+              <CardContent className="flex flex-col">
+                <div className="flex gap-5">
+                  <Image
+                    src={book.cover_url}
+                    alt={`Imagem do livro ${book.name}`}
+                    width={64}
+                    height={94}
+                  />
+                  <div className="flex flex-col">
+                    <h2 className="font-bold text-gray-100">{book.name}</h2>
+                    <p className="text-xs text-gray-400">{book.author}</p>
+                    <div className="mt-auto">
+                      <Rate size={5} value={ratingValue} />
+                    </div>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
